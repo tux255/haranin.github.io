0bb996cd6fee34c024e28bedc58f56e861611c56
Andrii Haranin <tux255@gmail.com>
2020-08-26 10:32:11 +0300

    Updates

---
+++ precache-manifest.be4ea2c3736dd129c197b740d3382af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132004e8428eab99a7908e309ac82fbf",
+    "url": "/haranin.github.io/index.html"
+  },
+  {
+    "revision": "779c4f3ea0c4cc115bac",
+    "url": "/haranin.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a059e47d2437d9f4d03",
+    "url": "/haranin.github.io/static/js/2.fb24facc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haranin.github.io/static/js/2.fb24facc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "779c4f3ea0c4cc115bac",
+    "url": "/haranin.github.io/static/js/main.564b6881.chunk.js"
+  },
+  {
+    "revision": "7f29e479cde83cdba673",
+    "url": "/haranin.github.io/static/js/runtime-main.9c96e4ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/haranin.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
